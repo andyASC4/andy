0f9f7eb32970b175d1d3cd06186e8ecdce331ee7
andyASC4 <linandy324@gmail.com>
2017-08-17 10:27:36 -0700

    FINAL/NIKKOS HAS FINAL CODE

---
+++ StreetWear Project/StreetwearConnect/blogPage/blogPage/sketch.js
@@ -0,0 +1,44 @@
+
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+
+console.log(img);
+img.onclick = function(){
+    console.log('myimg click');
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+    modal.style.display = "none";
+}
+
+// Next image example //
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img2 = document.getElementById('myImg2');
+var modalImg = document.getElementById("img01");
+
+
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+    modal.style.display = "none";
+}
